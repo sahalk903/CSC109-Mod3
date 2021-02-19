0ad3aa47d1b383d95679d3571945777343101568
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-19 12:55:30 -0500

    second push

--- src/Snack.java
+++ src/Snack.java
@@ -1,17 +1,13 @@
 public class Snack {
-public int price;
+	public int price;
 
-		public Snack(int price) {
-		this.price=price;
-	
-		
+	public Snack(int price) {
+		this.price = price;
+
 	}
-		
-  }
 	
+	public int getPrice() {
+		return price;
+	}
 
-	
-
-
-
-
+}
